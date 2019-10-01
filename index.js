a5c0c77be892386973d841eebdf545edc8c481d5
vraxzeztan <anusarsoni88@gmail.com>
2019-10-01 23:38:53 +0530

    added routes

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 app = express();
 
+var TodoRoutes = require('./routes/todos');
+
+
 app.get('/',function (req,res) {
     res.send({message: "hi!"});
 })
+
+app.use('/api/todos',TodoRoutes);
 
 app.listen(3000,function () {
     console.log('server running');
