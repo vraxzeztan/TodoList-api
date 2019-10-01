ad59ff3d0781c05a7afb25e759485979d9cdddba
vraxzeztan <anusarsoni88@gmail.com>
2019-10-01 23:42:47 +0530

    modified todos.js

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+
 
 router.get('/',function (req,res) {
-    res.send('Hi!');     
+    db.Todo.find()
+    .then(function (todos) {
+        res.json(todos); 
+    })
+    .catch(function(err) {
+        res.send(err);
+    })
 })
 
 module.exports = router;
