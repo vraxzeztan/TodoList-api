d890ea415f70f5b81775049af26ce0735801bcf6
vraxzeztan <anusarsoni88@gmail.com>
2019-10-02 12:36:03 +0530

    added post request

--- routes/todos.js
+++ routes/todos.js
@@ -13,7 +13,13 @@ router.get('/',function (req,res) {
 });
 
 router.post('/',function (req,res) {
-    
+    db.Todo.create(req.body)
+    .then(function(newTodo){
+        res.json(newTodo);
+    })
+    .catch(function(err){
+        res.send(err);
+    })
 })
 
 module.exports = router;
