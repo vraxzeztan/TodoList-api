154c044f20af6406c73e1ee4e596cd8de87c9f1d
vraxzeztan <anusarsoni88@gmail.com>
2019-10-02 12:35:34 +0530

    added body-parser

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 app = express();
+var bodyParser = require('body-parser');
 
 var TodoRoutes = require('./routes/todos');
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',function (req,res) {
     res.send({message: "hi!"});
