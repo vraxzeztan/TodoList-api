1a31d1fc9fd1777605fb0c000ca12937c211930d
vraxzeztan <anusarsoni88@gmail.com>
2019-10-03 18:25:09 +0530

    added html

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var bodyParser = require('body-parser');
 var TodoRoutes = require('./routes/todos');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(express.static(__dirname+'/views'));
 app.get('/',function (req,res) {
-    res.send({message: "hi!"});
+    res.sendFile('index.html');
 })
 
 app.use('/api/todos',TodoRoutes);
 
-app.listen(8000,function () {
+app.listen(3000,function () {
     console.log('server running');
 });
 
