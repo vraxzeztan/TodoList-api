3357ac888bfa0d3ef65f64ba2dc201540b0978fe
vraxzeztan <anusarsoni88@gmail.com>
2019-10-03 18:41:22 +0530

    added public + views

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var express = require('express');
 app = express();
 var bodyParser = require('body-parser');
-
 var TodoRoutes = require('./routes/todos');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname+'/public'));
 app.use(express.static(__dirname+'/views'));
+app.use('/api/todos',TodoRoutes);
+
 app.get('/',function (req,res) {
     res.sendFile('index.html');
 })
-
-app.use('/api/todos',TodoRoutes);
 
 app.listen(3000,function () {
     console.log('server running');
