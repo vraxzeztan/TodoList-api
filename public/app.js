56ae20399ae005c6ceeeab49e0008198ffba604c
vraxzeztan <anusarsoni88@gmail.com>
2019-10-04 13:40:20 +0530

    added create and add functionality

--- public/app.js
+++ public/app.js
@@ -1,1 +1,39 @@
-alert('hi!');
+$(document).ready(function(){
+    $.getJSON('/api/todos')
+    .then(addTodos)
+    .catch(function(err){
+        console.log(err);
+    });
+
+    $('#todoInput').keypress(function(event){
+        if(event.which == 13){
+            createTodo();
+        }
+    })
+});
+
+
+function addTodos(todos){
+   todos.forEach(function(todo){
+       addTodo(todo);
+   });
+}
+
+function addTodo(todo){
+    var newTodo = $('<li class="task">' + todo.name + '</li>');
+    if(todo.completed){
+        newTodo.addClass('done');
+    }
+    $('.list').append(newTodo);
+ }
+function createTodo(){
+    var usrInput = $('#todoInput').val();
+    $.post('/api/todos',{name : usrInput})
+    .then(function(newTodo){
+        $('#todoInput').val('');
+        addTodo( newTodo);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
