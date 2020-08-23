9d2956cfa2f09d146165557ac18ff9d6424188c0
vraxzeztan <anusarsoni88@gmail.com>
2020-08-23 12:20:21 +0530

    updated port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/',function (req,res) {
     res.sendFile('index.html');
 })
 
-app.listen(3000,function () {
+app.listen(process.env.PORT || 3000,function () {
     console.log('server running');
 });
 
