31936fd55894b1b0b375d9d0beb41306bf7832ea
vraxzeztan <anusarsoni88@gmail.com>
2020-08-23 12:03:23 +0530

    updated mongo uri

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo-api');
 
 mongoose.Promise = Promise;
 module.exports.Todo = require('./todo');
+    
